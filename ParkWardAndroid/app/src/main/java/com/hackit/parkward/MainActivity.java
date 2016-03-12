692c0f54bfd00379995d65a197aa6702f0e565dc
aleixpb2 <aleixpb2@gmail.com>
2016-03-12 10:29:12 +0100

    Configure dialog finished

--- ParkWardAndroid/app/src/main/java/com/hackit/parkward/MainActivity.java
+++ ParkWardAndroid/app/src/main/java/com/hackit/parkward/MainActivity.java
@@ -1,35 +1,74 @@
 package com.hackit.parkward;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    Button startButton;
+
     ImageView imageTaken;
-    boolean configured;
+    Button startButton;
+    Button configureButton;
+    int cameraId = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Find views
-        startButton = (Button) findViewById(R.id.Start_Button);
         imageTaken = (ImageView) findViewById(R.id.Image_Taken);
+        startButton = (Button) findViewById(R.id.Start_Button);
+        configureButton = (Button) findViewById(R.id.Configure_Button);
 
         // On Click Listeners
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dispatchTakePictureIntent();
+                if(cameraId != 0) {
+                    Log.d("Start", "Camera id: " + cameraId);
+                    dispatchTakePictureIntent();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "First enter your id", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        configureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("ConfigureButton", "Clicked");
+                final EditText editText = new EditText(MainActivity.this);
+                editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("ID");
+                builder.setMessage("Enter your ID");
+                builder.setView(editText);
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        cameraId = Integer.parseInt(editText.getText().toString());
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
             }
         });
     }
@@ -40,7 +79,6 @@ public class MainActivity extends AppCompatActivity {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
@@ -49,5 +87,4 @@ public class MainActivity extends AppCompatActivity {
             imageTaken.setImageBitmap(imageBitmap);
         }
     }
-
 }
