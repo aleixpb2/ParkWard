8bc7910c6e1b4e720b927999e69018003e76f8be
aleixpb2 <aleixpb2@gmail.com>
2016-03-12 11:39:16 +0100

    Photos are saved

--- ParkWardAndroid/app/src/main/java/com/hackit/parkward/MainActivity.java
+++ ParkWardAndroid/app/src/main/java/com/hackit/parkward/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,14 +18,22 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
 
-    ImageView imageTaken;
-    Button startButton;
-    Button configureButton;
-    int cameraId = 0;
+    private ImageView imageTaken;
+    private Button startButton;
+    private Button configureButton;
+    private int cameraId = 0;
+    private String lastPhotoString = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,7 +70,9 @@ public class MainActivity extends AppCompatActivity {
                 builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        cameraId = Integer.parseInt(editText.getText().toString());
+                        if(editText.getText().length() != 0)
+                            cameraId = Integer.parseInt(editText.getText().toString());
+                        else Toast.makeText(MainActivity.this, "Empty id, try again", Toast.LENGTH_SHORT).show();
                     }
                 });
                 builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -75,16 +88,66 @@ public class MainActivity extends AppCompatActivity {
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                Log.e("dispatchTakePicture","Error while creating the File");
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
         }
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageTaken.setImageBitmap(imageBitmap);
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            imageTaken.setImageBitmap(imageBitmap);
+            DEBUGsetImageTaken();
+            SendImage();
         }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        lastPhotoString = /*"file:" +*/ image.getAbsolutePath();
+        Log.d("createImageFile","lastPhotoString: " + lastPhotoString);
+        return image;
+    }
+
+    private void DEBUGsetImageTaken(){
+        Bitmap bitmap=null;
+        File f= new File(lastPhotoString);
+        Log.d("setImage", f.toString());
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+        try {
+            bitmap = BitmapFactory.decodeStream(new FileInputStream(f), null, options);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        imageTaken.setImageBitmap(bitmap);
+    }
+
+    private void SendImage(){
+        
     }
 }
