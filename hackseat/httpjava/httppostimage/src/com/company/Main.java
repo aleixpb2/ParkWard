2606100d0298fc69d69a84ac3d91edace8f577a1
estevetarra <ets1995@hotmail.com>
2016-03-16 19:48:48 +0100

    Added the camera part

--- hackseat/httpjava/httppostimage/src/com/company/Main.java
+++ hackseat/httpjava/httppostimage/src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import com.github.sarxos.webcam.Webcam;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpVersion;
@@ -12,13 +13,39 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.CoreProtocolPNames;
 import org.apache.http.util.EntityUtils;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
+import java.util.Scanner;
 
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        System.out.println("Enter the ID of the camera: ");
+        int ID = reader.nextInt(); // Scans the next token of the input as an int.
+        String aux = new String();
+        aux.concat(String.valueOf(ID));
+        aux.concat("_");
+        aux.concat(String.valueOf(new Date().getTime()));
+        aux.concat(".png");
+
+
+
+
+        // get default webcam and open it
+        Webcam webcam = Webcam.getDefault();
+        webcam.open();
+
+        // get image
+        BufferedImage image = webcam.getImage();
+
+        // save image to PNG file
+        ImageIO.write(image, "PNG", new File(aux));
 
         HttpClient httpclient = new DefaultHttpClient();
         httpclient.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
