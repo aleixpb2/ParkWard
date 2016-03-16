0cee9b154f8e265ee6bd08cae15bb0931e17c739
estevetarra <ets1995@hotmail.com>
2016-03-16 20:14:47 +0100

    Made the photo periodically, missing the deleting part

--- hackseat/httpjava/httppostimage/src/com/company/Main.java
+++ hackseat/httpjava/httppostimage/src/com/company/Main.java
@@ -23,54 +23,61 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         Scanner reader = new Scanner(System.in);  // Reading from System.in
         System.out.println("Enter the ID of the camera: ");
         int ID = reader.nextInt(); // Scans the next token of the input as an int.
-        String aux = new String();
-        aux.concat(String.valueOf(ID));
-        aux.concat("_");
-        aux.concat(String.valueOf(new Date().getTime()));
-        aux.concat(".png");
+        System.out.println("Enter the period: ");
+        int periode = reader.nextInt(); // Scans the next token of the input as an int.
+
+        while (true) {
+            String aux = new String();
 
 
+            aux += "./";
+            aux += String.valueOf(ID);
+            aux += "_";
+            aux += (String.valueOf(new Date().getTime()));
+            aux += ".png";
 
 
-        // get default webcam and open it
-        Webcam webcam = Webcam.getDefault();
-        webcam.open();
+            // get default webcam and open it
+            Webcam webcam = Webcam.getDefault();
+            webcam.open();
 
-        // get image
-        BufferedImage image = webcam.getImage();
+            // get image
+            BufferedImage image = webcam.getImage();
 
-        // save image to PNG file
-        ImageIO.write(image, "PNG", new File(aux));
+            // save image to PNG file
+            ImageIO.write(image, "PNG", new File(aux));
 
-        HttpClient httpclient = new DefaultHttpClient();
-        httpclient.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
+            HttpClient httpclient = new DefaultHttpClient();
+            httpclient.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
 
-        HttpPost httppost = new HttpPost("http://bestbarcelona.org/externalprojects/hackseat/server/upload.php");
-        File file = new File("C:/Users/ets19/Desktop/seat/42.png");
+            HttpPost httppost = new HttpPost("http://bestbarcelona.org/externalprojects/hackseat/server/upload.php");
+            File file = new File(aux);
 
-        MultipartEntity mpEntity = new MultipartEntity();
-        ContentBody cbFile = new FileBody(file, "image/png");
-        mpEntity.addPart("userfile", cbFile);
+            MultipartEntity mpEntity = new MultipartEntity();
+            ContentBody cbFile = new FileBody(file, "image/png");
+            mpEntity.addPart("userfile", cbFile);
 
 
-        httppost.setEntity(mpEntity);
-        System.out.println("executing request " + httppost.getRequestLine());
-        HttpResponse response = httpclient.execute(httppost);
-        HttpEntity resEntity = response.getEntity();
+            httppost.setEntity(mpEntity);
+            System.out.println("executing request " + httppost.getRequestLine());
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity resEntity = response.getEntity();
 
-        System.out.println(response.getStatusLine());
-        if (resEntity != null) {
-            System.out.println(EntityUtils.toString(resEntity));
+            System.out.println(response.getStatusLine());
+            if (resEntity != null) {
+                System.out.println(EntityUtils.toString(resEntity));
+            }
+            if (resEntity != null) {
+                resEntity.consumeContent();
+            }
+
+            httpclient.getConnectionManager().shutdown();
+            Thread.sleep(periode * 1000);
         }
-        if (resEntity != null) {
-            resEntity.consumeContent();
-        }
-
-        httpclient.getConnectionManager().shutdown();
     }
 }
